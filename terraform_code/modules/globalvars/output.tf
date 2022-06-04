78a810eae7538334c14f4efbc77a1135e8b3cc60
Rajvi Khatri <khatrirajvi@gmail.com>
2022-06-04 15:52:21 -0400

    first commit of terraform

---
+++ terraform_code/modules/globalvars/output.tf
@@ -0,0 +1,13 @@
+# Default tags
+output "default_tags" {
+  value = {
+    "Owner" = "Rajvi"
+    "App"   = "Web"
+    "Project" = "CLO835"
+  }
+}
+
+# Prefix to identify resources
+output "prefix" {
+  value = "assign"
+}
